b0f8774ad82f766bbb9c32010a6a3858ec023ec5
Edoardo Carpita <edoardo.carpita@depsys.ch>
2019-06-10 11:17:29 +0200

    labo 2b final

--- express-image/src/index.js
+++ express-image/src/index.js
@@ -3,8 +3,20 @@ var chance = new Chance();
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send(generateStudents());
+app.get('/', function (req, res) {	
+  res.send(generateZooAnimals());
+});
+
+app.get('/zoo', function (req, res) {	
+  res.send(generateZooAnimals());
+});
+
+app.get('/pet', function (req, res) {	
+  res.send(generatePetAnimals());
+});
+
+app.get('/farm', function (req, res) {	
+  res.send(generateFarmAnimals());
 });
 
 app.get('/test', function (req, res) {
@@ -16,14 +28,46 @@ app.listen(3000, function () {
 });
 
 
-function generateStudents(){
-	var students = [];
-	for (var i = 0; i < 4; i++){
-		students.push({
-		firstName : chance.first(),
-		lastName : chance.last()		
+function generateAnimals(){
+	var animals = [];
+	for (var i = 0; i < 10; i++){
+		animals.push({
+		animal : chance.animal()
 		});		
 	};	
-	console.log(students);
-	return students;
+	console.log(animals);
+	return animals;
+};
+
+function generateZooAnimals(){
+	var animals = [];
+	for (var i = 0; i < 10; i++){
+		animals.push({
+		animal : chance.animal({type: 'zoo'})
+		});		
+	};	
+	console.log(animals);
+	return animals;
+};
+
+function generatePetAnimals(){
+	var animals = [];
+	for (var i = 0; i < 10; i++){
+		animals.push({
+		animal : chance.animal({type: 'pet'})
+		});		
+	};	
+	console.log(animals);
+	return animals;
+};
+
+function generateFarmAnimals(){
+	var animals = [];
+	for (var i = 0; i < 10; i++){
+		animals.push({
+		animal : chance.animal({type: 'farm'})
+		});		
+	};	
+	console.log(animals);
+	return animals;
 };
